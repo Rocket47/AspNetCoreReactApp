d574cb312ee937a25973bf724fe1d4858c94062e
Rocket47 <roman.tsenov@gmail.com>
2022-12-27 23:37:20 +0600

    Add create, post, put methods

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -1,35 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
+        public ActivitiesController()
         {
-            _mediator = mediator;
+
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities() 
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
         }
     }
 }
